51b5c973494500d4f7e48f8d849edde6ce32dcc5
elias-hossen <github.com/elias-hossen>
2023-05-20 23:50:19 +0600

    to do list 2

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const inputText = document.querySelector('.user-text');
 const addBtn = document.querySelector('.btn-add');
 const textArea = document.querySelector('.text-area');
 
-addBtn.addEventListener('click', function () {
-  const text = inputText.value;
+const work = function () {
+  const text =
+    inputText.value[0].toUpperCase() +
+    inputText.value.slice(1, inputText.value.lenght);
   if (inputText.value === '') {
     alert('Please write something...');
   } else {
@@ -21,11 +23,20 @@ addBtn.addEventListener('click', function () {
     textArea.appendChild(newItem);
     inputText.value = '';
   }
-});
+};
+
+addBtn.addEventListener('click', work);
+
 textArea.addEventListener('click', function (e) {
   if (e.target.classList.contains('fa-x'))
     e.target.parentElement.classList.add('hidden');
   else if (e.target.classList.contains('fa-check')) {
     e.target.parentElement.classList.toggle('complete');
+  }
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    work();
   }
 });
